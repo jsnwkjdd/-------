a60ca57c23ea0ca9ecc2ee7717a7c14b9f029ed0
jsnwkjdd <1965584947@qq.com>
2025-11-01 12:46:20 +0800

    配置了定时中断，电机1的编码器，驱动

--- System/Timer.c
+++ System/Timer.c
@@ -1,41 +1,41 @@
 #include "stm32f10x.h"                  // Device header
-
+//10ms¶¨Ê±
 void Timer_Init(void)
 {
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
 	
-	TIM_InternalClockConfig(TIM2);
+	TIM_InternalClockConfig(TIM1);
 	
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
 	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
 	TIM_TimeBaseInitStructure.TIM_Period = 10000 - 1;
-	TIM_TimeBaseInitStructure.TIM_Prescaler = 7200 - 1;
+	TIM_TimeBaseInitStructure.TIM_Prescaler = 72 - 1;
 	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
-	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure);
+	TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitStructure);
 	
-	TIM_ClearFlag(TIM2, TIM_FLAG_Update);
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+	TIM_ClearFlag(TIM1, TIM_FLAG_Update);
+	TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
 	
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	
 	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannel = TIM1_UP_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
 	NVIC_Init(&NVIC_InitStructure);
 	
-	TIM_Cmd(TIM2, ENABLE);
+	TIM_Cmd(TIM1, ENABLE);
 }
 
 /*
-void TIM2_IRQHandler(void)
+void TIM1_UP_IRQHandler(void)
 {
-	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+	if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
 	{
 		
-		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+		TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
 	}
 }
 */
