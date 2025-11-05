85b9ab7407b973937e4c3ea1aeac771bc69efb9d
jsnwkjdd <1965584947@qq.com>
2025-11-05 14:55:07 +0800

    终稿

--- User/main.c
+++ User/main.c
@@ -25,49 +25,63 @@ int main(void)
 	Encoder_Init2();
 	Serial_Init();
 	Timer_Init();
-	OLED_Printf(0, 0, OLED_8X16, "Speed Control");
-	OLED_Update();
 	Target=0;
 	Kp=5, Ki=1, Kd=3;
+	OLED_Printf(0, 0, OLED_8X16, "Speed Control");
+	OLED_Printf(0, 16, OLED_8X16, "Kp:%4.2f", Kp);	
+	OLED_Printf(0, 32, OLED_8X16, "Ki:%4.2f", Ki);	
+	OLED_Printf(0, 48, OLED_8X16, "Kd:%4.2f", Kd);	
+	OLED_Update();
 	while (1)
 	{
 		KeyNum = Key_GetNum();
 		if(KeyNum==1)
 		{
+			Motor_SetSpeed(0);
+			Motor2_SetSpeed(0);
 			Target=0;
 			Actual=0;
 			Out = 0;
 			Error0 = 0;
 			Error1 = 0;
 			Error2 = 0;
-
 			p=1-p; 
 			OLED_Clear();
 			OLED_Update();
+			if(p==0)
+			{	
+				Kp=7, Ki=1.8, Kd=2;
+				OLED_Printf(0, 0, OLED_8X16, "Speed Control");
+				OLED_Printf(0, 16, OLED_8X16, "Kp:%4.2f", Kp);	
+				OLED_Printf(0, 32, OLED_8X16, "Ki:%4.2f", Ki);	
+				OLED_Printf(0, 48, OLED_8X16, "Kd:%4.2f", Kd);	
+				OLED_Printf(64, 16, OLED_8X16, "Tar:%+04.0f", Target);
+				OLED_Printf(64, 32, OLED_8X16, "Act:%+04.0f", Actual);
+				OLED_Printf(64, 48, OLED_8X16, "Out:%+04.0f", Out);	
+				OLED_Update();
+			}
+			else{
+					Kp=1.2, Ki=0.13, Kd=0.1;
+					OLED_Printf(0, 0, OLED_8X16, "Location Control");
+					OLED_Printf(0, 16, OLED_8X16, "Kp:%4.2f", Kp);	
+					OLED_Printf(0, 32, OLED_8X16, "Ki:%4.2f", Ki);	
+					OLED_Printf(0, 48, OLED_8X16, "Kd:%4.2f", Kd);
+					OLED_Update();
+			}
 		}
 		if(p==0)
-		{	
-			Kp=5, Ki=2, Kd=3;
-			OLED_Printf(0, 0, OLED_8X16, "Speed Control");
-			OLED_Printf(0, 16, OLED_8X16, "Kp:%4.2f", Kp);	
-			OLED_Printf(0, 32, OLED_8X16, "Ki:%4.2f", Ki);	
-			OLED_Printf(0, 48, OLED_8X16, "Kd:%4.2f", Kd);	
-			OLED_Printf(64, 16, OLED_8X16, "Tar:%+04.0f", Target);
-			OLED_Printf(64, 32, OLED_8X16, "Act:%+04.0f", Actual);
-			OLED_Printf(64, 48, OLED_8X16, "Out:%+04.0f", Out);	
-			OLED_Update();
-		}
+			{	
+				OLED_Printf(64, 16, OLED_8X16, "Tar:%+04.0f", Target);
+				OLED_Printf(64, 32, OLED_8X16, "Act:%+04.0f", Actual);
+				OLED_Printf(64, 48, OLED_8X16, "Out:%+04.0f", Out);	
+				OLED_Update();
+			}
 		else{
-			Kp=3, Ki=0.1 , Kd=0;
-			OLED_Printf(0, 0, OLED_8X16, "Location Control");
-			OLED_Printf(0, 16, OLED_8X16, "Kp:%4.2f", Kp);	
-			OLED_Printf(0, 32, OLED_8X16, "Ki:%4.2f", Ki);	
-			OLED_Printf(0, 48, OLED_8X16, "Kd:%4.2f", Kd);
 			OLED_Printf(64, 16, OLED_8X16, "Tar:%+04.0f", Target);
 			OLED_Printf(64, 32, OLED_8X16, "Act:%+04.0f", Actual);
 			OLED_Printf(64, 48, OLED_8X16, "Out:%+04.0f", Out);	
 			OLED_Update();
-		}
+			}
 		Serial_Printf("%f,%f,%f\r\n", Target, Actual, Out);	
 		if(Serial_RxFlag == 1)
 		{
@@ -84,8 +98,7 @@ void TIM1_UP_IRQHandler(void)
 	{
 		Count ++;
 		Key_Tick();	
-		if (Count >= 10)	
-		{
+		if (Count >= 10){
 			Count = 0;	
 			if(p==0){
 				Actual = Encoder_Get();
@@ -99,7 +112,7 @@ void TIM1_UP_IRQHandler(void)
 				Motor_SetSpeed(Out);
 			}
 			else
-			{
+			{	
 				Target+=Encoder_Get();
 				Actual+=Encoder_Get2();
 				Error2 = Error1;
